61fddbadb1f0c8270eaa10df4f2481cd21b3a718
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-03 02:42:47 +0700

    add relation to Brand & ProductType

--- models/ProductType.js
+++ models/ProductType.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      ProductType.hasMany(models.Product, {
+        foreignKey: "productTypeId",
+        as: "products",
+      });
     }
   }
   ProductType.init(
