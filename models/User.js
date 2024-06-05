51a55a7344303f77eeb0811dae61e6f528846e4e
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-05 23:27:46 +0700

    refactor User model

--- models/User.js
+++ models/User.js
@@ -1,62 +1,72 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require(".");
-
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-          },
-          name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          password: {
-            type: DataTypes.STRING,
-            allowNull: true,
-          },
-          role: {
-            type: DataTypes.ENUM,
-            allowNull: false,
-            values: ["admin", "member"],
-            defaultValue: "member",
-          },
-          birthdate: {
-            type: DataTypes.DATE,
-            allowNull: true,
-          },
-          gender: {
-            type: DataTypes.ENUM,
-            allowNull: true,
-            values: ["male", "female"],
-          },
-          profile_picture: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            field: "profile_picture",
-          },
-          createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-          },
-          updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-          },
-    }, 
-    {
-        tableName: "users",
-        timestamps: true,
-        paranoid: true,
-        deletedAt: "deletedAt",
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-);
-return User;
-
-}
+  }
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        values: ["admin", "member"],
+        defaultValue: "member",
+      },
+      birthdate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      gender: {
+        type: DataTypes.ENUM,
+        allowNull: true,
+        values: ["male", "female"],
+      },
+      profile_picture: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "profile_picture",
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "users",
+      timestamps: true,
+      paranoid: true,
+      deletedAt: "deletedAt",
+    }
+  );
+  return Product;
+};
