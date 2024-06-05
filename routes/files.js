d69d32fcc2a02d86d24bd1f309317ab7ea4ad391
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-05 21:36:18 +0700

    add verify token on upload

--- routes/files.js
+++ routes/files.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const fileController = require("../controllers/fileController");
 const multer = require("multer");
+const verifyToken = require("../middlewares/verify-token");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post("/", upload.single("image"), fileController.upload);
+router.post("/", upload.single("image"), verifyToken, fileController.upload);
 
 module.exports = router;
