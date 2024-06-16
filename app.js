77c2d035ce26881aecbd4814d17e1a5c5078d42e
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-16 13:39:21 +0700

    load Skintype Prediction Model & Skincare Recommendation System Model

--- app.js
+++ app.js
@@ -1,52 +1,67 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const dotenv = require("dotenv");
 
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const usersRouter = require('./routes/users');
-const articlesRouter = require('./routes/articles');
-const brandsRouter = require('./routes/brands');
-const productTypesRouter = require('./routes/productTypes');
-const productsRouter = require('./routes/products');
-const filesRouter = require('./routes/files');
-const utilsRouter = require('./routes/utils');
-const profileRouter = require('./routes/profile');
-const savedProductsRouter = require('./routes/savedProducts');
-// const predictRouter = require('./routes/predict');
+const {
+  loadPredictionModel,
+  loadRecommendationModel,
+} = require("./services/loadModel");
+
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const usersRouter = require("./routes/users");
+const articlesRouter = require("./routes/articles");
+const brandsRouter = require("./routes/brands");
+const productTypesRouter = require("./routes/productTypes");
+const productsRouter = require("./routes/products");
+const filesRouter = require("./routes/files");
+const utilsRouter = require("./routes/utils");
+const profileRouter = require("./routes/profile");
+const savedProductsRouter = require("./routes/savedProducts");
+const predictRouter = require("./routes/predict");
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 3300;
 
-app.use(logger('dev'));
+let predictionModel;
+let recommendationModel;
+
+(async () => {
+  predictionModel = await loadPredictionModel();
+  recommendationModel = await loadRecommendationModel();
+  app.locals.predictionModel = predictionModel;
+  app.locals.recommendationModel = recommendationModel;
+})();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/users', usersRouter);
-app.use('/articles', articlesRouter);
-app.use('/brands', brandsRouter);
-app.use('/product-types', productTypesRouter);
-app.use('/products', productsRouter);
-app.use('/files', filesRouter);
-app.use('/utils', utilsRouter);
-app.use('/profile', profileRouter);
-app.use('/saved-products', savedProductsRouter);
-// app.use('/predict', predictRouter);
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+app.use("/articles", articlesRouter);
+app.use("/brands", brandsRouter);
+app.use("/product-types", productTypesRouter);
+app.use("/products", productsRouter);
+app.use("/files", filesRouter);
+app.use("/utils", utilsRouter);
+app.use("/profile", profileRouter);
+app.use("/saved-products", savedProductsRouter);
+// app.use("/predict", predictRouter);
 
 app.listen(PORT, () => {
   console.log("Express API running in port: " + PORT);
--- services/loadModel.js
+++ services/loadModel.js
@@ -1,9 +1,28 @@
-// const tf = require('@tensorflow/tfjs-node');
-// const dotenv = (require("dotenv").config()).parsed
+const tf = require("@tensorflow/tfjs-node");
 
-// async function loadModel() {
-//   const model = await tf.loadLayersModel(dotenv.MODEL_URL);
-//   return model;
-// }
+async function loadPredictionModel() {
+  try {
+    model = await tf.loadGraphModel(process.env.SKINTYPE_PREDICTION_MODEL_URL);
+    console.log("Skintype Prediction Model loaded successfully");
+    return model;
+  } catch (err) {
+    console.error("Error loading Skintype Prediction Model", err);
+  }
+}
 
-// module.exports = loadModel;
+async function loadRecommendationModel() {
+  try {
+    model = await tf.loadGraphModel(
+      process.env.SKINCARE_RECOMMENDATION_MODEL_URL
+    );
+    console.log("Recommendation Model loaded successfully");
+    return model;
+  } catch (err) {
+    console.error("Error loading Skincare Recommendation Model", err);
+  }
+}
+
+module.exports = {
+  loadPredictionModel,
+  loadRecommendationModel,
+};
