3e79cbb9310f6ba5148f5ea96bfe343af40a65f6
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-16 14:48:03 +0700

    creating seed skincare product types based on Skincare Recommendation System dataset

--- seeders/20240601095454-initial-product-types.js
+++ seeders/20240601095454-initial-product-types.js
@@ -7,67 +7,32 @@ module.exports = {
       "product_types",
       [
         {
-          name: "Cleanser",
-          description:
-            "Digunakan untuk membersihkan wajah dari kotoran, debu, dan minyak",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Makeup remover",
-          description: "Digunakan untuk membersihkan makeup dari wajah",
+          name: "Face Wash",
+          description: "",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Toner",
-          description:
-            "Digunakan untuk menyegarkan wajah dan menyeimbangkan pH kulit",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Exfoliator",
-          description:
-            "Digunakan untuk mengangkat sel kulit mati dan membuat kulit lebih halus",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Masker",
-          description:
-            "Digunakan untuk memberikan nutrisi dan perawatan ekstra pada kulit",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Moisturizer",
-          description: "Digunakan untuk menjaga kelembapan kulit",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Sunblock",
-          description: "Digunakan untuk melindungi kulit dari sinar matahari",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Eye cream",
-          description: "Digunakan untuk merawat kulit di sekitar mata",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Lip balm",
-          description: "Digunakan untuk melembabkan bibir",
+          description: "",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Serum",
-          description:
-            "Digunakan untuk mengatasi masalah kulit tertentu, seperti jerawat, keriput, atau flek hitam",
+          description: "",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Moisturizer",
+          description: "",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Sunscreen",
+          description: "",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
