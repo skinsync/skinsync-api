f318fe22e2ffa0653644060533e0f2c79308a1c7
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-19 21:29:47 +0700

    fix upload gcs

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -6,12 +6,7 @@ exports.upload = async (req, res) => {
   }
 
   const { folder } = req.query;
-
-  if (folder) {
-    var folderPath = folder + "/";
-  } else {
-    var folderPath = "";
-  }
+  const folderPath = folder ? `${folder}/` : "";
 
   try {
     const publicUrl = await uploadToCloudStorage(req.file, folderPath);
@@ -22,23 +17,4 @@ exports.upload = async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-
-  // const blob = bucket.file(
-  //   folderPath + Date.now() + "_" + req.file.originalname
-  // );
-  // const blobStream = blob.createWriteStream({
-  //   resumable: false,
-  //   gzip: true,
-  // });
-
-  // blobStream.on("error", (err) => {
-  //   res.status(500).json({ message: err.message });
-  // });
-
-  // blobStream.on("finish", () => {
-  //   const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-  //   res.status(200).send({ url: publicUrl });
-  // });
-
-  // blobStream.end(req.file.buffer);
 };
--- services/uploadCloudStorage.js
+++ services/uploadCloudStorage.js
@@ -4,24 +4,24 @@ const gc = new Storage();
 const bucket = gc.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 async function uploadToCloudStorage(file, folderPath = "") {
-  const blob = bucket.file(
-    folderPath + Date.now() + "_" + file.originalname
-  );
-  const blobStream = blob.createWriteStream({
-    resumable: false,
-    gzip: true,
-  });
+  return new Promise((resolve, reject) => {
+    const blob = bucket.file(folderPath + Date.now() + "_" + file.originalname);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      gzip: true,
+    });
 
-  blobStream.on("error", (err) => {
-    return err.message;
-  });
+    blobStream.on("error", (err) => {
+      reject(err.message);
+    });
 
-  blobStream.on("finish", () => {
-    const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-    return publicUrl;
-  });
+    blobStream.on("finish", () => {
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      resolve(publicUrl);
+    });
 
-  blobStream.end(file.buffer);
+    blobStream.end(file.buffer);
+  });
 }
 
 module.exports = uploadToCloudStorage;
